f6948507da9840db43d081be8c42beb63d155bc9
IlyanHaifi <ilyan.haifi@iut-dijon.u-bourgogne.fr>
2026-01-29 00:36:17 +0100

    Delete parallaxe directory

--- parallaxe/export/import/import.mjs
+++
@@ -1,7 +0,0 @@
-import defaut, { jeSuisUnExportNomme, jeSuisUnExportRenomme as renomme } from './export.mjs';
-
-console.log(defaut());// Je suis l'export par défaut.
-
-console.log(jeSuisUnExportNomme());// Je suis un export nommé.
-
-console.log(renomme());// Je suis un export nommé dont l'import est renommé.
